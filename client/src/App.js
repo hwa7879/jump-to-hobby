3f74862e7d1b82034ff453cca68ebbcdfddbfc1e
남경화 <hwahwa7879@gmail.com>
2021-11-15 17:28:59 +0900

    Create 메인페이지구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Mainpage from "./pages/Mainpage";
 
-function App() {
+const button_style = {
+  position: "absolute",
+  zIndex: 1,
+};
+
+const content_box = {
+  position: "relative",
+  zIndex: 2,
+  backgroundColor: "skyblue",
+  padding: "120px",
+};
+
+export const Logo = styled.img`
+  width: 200px;
+  height: 200px;
+  cursor: pointer;
+`;
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logo src="/images/logo.png" />
+      <div style={button_style} onClick={() => console.log("clicked")}>
+        <button onClick={() => setIsOpen(true)}>Signup</button>
+        <button onClick={() => setIsOpen(true)}>Login / Logout</button>
+        <div style={content_box}>이미지추가</div>
+        <div style={content_box}>이미지추가</div>
+        <button>이미지게시판</button>
+        <button>업로드게시판</button>
+
+        <Mainpage open={isOpen} onClose={() => setIsOpen(false)}>
+          <div>
+            <input type="text" placeholder="email"></input>
+          </div>
+          <div>
+            <input type="text" placeholder="password"></input>
+          </div>
+        </Mainpage>
+      </div>
+    </>
   );
 }
-
-export default App;
