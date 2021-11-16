1604d2f758004cb54373e1518f2db650f725986a
jhpark9007 <jhpark9007@gmail.com>
2021-11-16 17:33:10 +0900

    이미지 컨트롤러 구현

--- server/controllers/images/imageDelete.js
+++ server/controllers/images/imageDelete.js
@@ -1,13 +1,13 @@
 const { posts_imgs } = require("../../models");
 
 module.exports = async (req, res) => {
-  const userinfo = req.params.id;
+  const userInfo = req.params.id;
 
-  if (!userinfo) {
-    res.satus(404).send({ message: "Not found" });
+  if (!userInfo) {
+    res.status(404).send({ message: "Not found" });
   }
   await posts_imgs.destroy({
-    where: { id: userinfo },
+    where: { id: userInfo },
   });
   return res.status(200).send({ message: "successfully deleted img" });
 };
