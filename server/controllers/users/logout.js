dd9b8f5b045b37de2a8e93cc436b36633e34d00f
dudgks5846 <dudgks5846@naver.com>
2021-11-17 10:52:47 +0900

    Update signout 수정

--- server/controllers/users/logout.js
+++ server/controllers/users/logout.js
@@ -1,16 +1,8 @@
 module.exports = (req, res) => {
-  res.cookie("login", false);
-  res.cookie("accessToken", null);
-  res.status(200).send("성공적으로 로그아웃 되셨습니다.");
+  if (req.session) {
+    req.session.destroy();
+    res.status(200).send({ message: "successfully signed out!" });
+  } else {
+    res.status(400).send({ message: "you're currently not logined" });
+  }
 };
-// res.cookie("login", false);
-// res.cookie("accessToken", null);
-// res.status(200).send("성공적으로 로그아웃 되셨습니다.");
-
-// const { accessToken } = req.body;
-
-// if(!accessToken) {
-//     res.status(400).send('bad request')
-// } else {
-//     res.status(200).send('logout success')
-// }
